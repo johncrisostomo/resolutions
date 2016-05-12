74415311503dd3d2c7ea2e4d8b294c8a7ea53240
John Crisostomo <jd@snapzio.com>
2016-05-12 13:11:51 +0800

    adds Bert for notifications

--- client/resolutions/ResolutionsForm.jsx
+++ client/resolutions/ResolutionsForm.jsx
@@ -4,10 +4,16 @@ export default class ResolutionsForm extends Component {
   addResolution(event) {
     event.preventDefault();
     var text = this.refs.resolution.value.trim();
-
-    Meteor.call('addResolution', text, () => {
-      this.refs.resolution.value= "";
-    });
+    if (text) {
+      Meteor.call('addResolution', text, (error, data) => {
+        if (error) {
+          Bert.alert('Please login before submitting', 'danger', 'fixed-top',
+            'fa-frown-o');
+        } else {
+          this.refs.resolution.value= "";
+        }
+      });
+    }
   }
 
   render() {
